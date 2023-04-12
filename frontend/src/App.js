d07188c2d1159a7571b725169c7da43735223322
brandon <brandon.l.merry@gmail.com>
2023-04-13 00:26:11 +0700

    created the footer element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
-    <div className='App'>
-      <>
-        <h1> invoicing app</h1>
-      </>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1> invoicing app</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
