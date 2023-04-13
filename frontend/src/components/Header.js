55a1e1e234f446ae01432f43a0b1270fd12b4e94
brandon <brandon.l.merry@gmail.com>
2023-04-13 17:14:17 +0700

    created a header with bootstrap

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
 
-const Header = () => {
-  return <header></header>
+function Header() {
+  return (
+    <header>
+      <Navbar bg='light' expand='lg' collapseOnSelect>
+        <Container>
+          <Navbar.Brand href='/'>Invoicing app</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
+                <i class='fa fa-shopping-cart'></i>
+                Get Invoice
+              </Nav.Link>
+              <Nav.Link>
+                <i className='fas fa-user'></i> login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
 }
 
 export default Header
