3f8958c4e0445ffc01281017dcfa468fb42ed4b9
brandon <brandon.l.merry@gmail.com>
2023-04-16 19:01:12 +0700

    added colors npm package

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import colors from 'colors'
 import products from './data/products.js'
 
 dotenv.config()
@@ -21,5 +22,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   PORT,
-  console.log(`is running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `is running in ${process.env.NODE_ENV} on port ${PORT}`.bgWhite.bold
+  )
 )
